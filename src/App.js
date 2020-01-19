538922dfd5ec0aeec2216a71208e2aa195f7b9af
Swain, Ashok <AshokKumar.Swain7@T-Mobile.com>
2020-01-19 14:33:07 -0800

    Updated the React Routing

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './components/pages/homepage/hompage.component';
+import {BrowserRouter,Switch, Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
